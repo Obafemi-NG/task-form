65a337e9256ae82c001d41b14e821931b26927b6
obafemi <oludahunsiobafemi18@gmail.com>
2022-02-01 11:32:17 +0100

    created a custom hook for fetching data from the Api and implemented it in the newTask component so that i dont have to write the logic of writing of adding new item to the database collection list

--- src/components/new-task/new-task.component.jsx
+++ src/components/new-task/new-task.component.jsx
@@ -1,37 +1,53 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Section from "../../UI/section/section.ui";
 import TaskForm from "../task-form/task-form.component";
+import useApi from "../../custom-hooks/useApi.hooks";
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest } = useApi();
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const url =
-        "https://task-form-project-default-rtdb.firebaseio.com/tasks.json";
-      const response = await fetch(url, {
-        method: "POST",
-        body: JSON.stringify({ text: taskText }),
-        headers: {
-          "content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Request Denied!");
-      }
-      const data = await response.json();
-      console.log(data);
-      const generatedId = data.name;
+    const addedData = (apiData) => {
+      const generatedId = apiData.name;
       const createdTask = { id: generatedId, text: taskText };
       props.onAddTask(createdTask);
       console.log(createdTask);
-    } catch (error) {
-      setError(error.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    };
+
+    sendRequest(
+      {
+        url: "https://task-form-project-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({ text: taskText }),
+      },
+      addedData
+    );
+
+    // try {
+    //   const url =
+    //     "https://task-form-project-default-rtdb.firebaseio.com/tasks.json";
+    //   const response = await fetch(url, {
+    //     method: "POST",
+    //     body: JSON.stringify({ text: taskText }),
+    //     headers: {
+    //       "content-Type": "application/json",
+    //     },
+    //   });
+    //   if (!response.ok) {
+    //     throw new Error("Request Denied!");
+    //   }
+    // const data = await response.json();
+    // console.log(data);
+    // const generatedId = data.name;
+    // const createdTask = { id: generatedId, text: taskText };
+    // props.onAddTask(createdTask);
+    // console.log(createdTask);
   };
+  //   } catch (error) {
+  //     setError(error.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
 
   return (
     <Section>
